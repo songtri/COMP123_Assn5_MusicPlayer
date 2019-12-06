5753df626ba2083abe41b3eb7eb16d0a65e32c5c
Song Wook <songtri@gmail.com>
2019-12-05 23:09:09 -0500

    예외 추가

--- MusicPlayer/Playlist.cs
+++ MusicPlayer/Playlist.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -66,41 +67,54 @@ namespace MusicPlayer
 
 		public Song LoadTagInfo(string filePath)
 		{
-			using (FileStream fs = File.OpenRead(filePath))
+			try
 			{
-				if (fs.Length >= 128)
+				using (FileStream fs = File.OpenRead(filePath))
 				{
-					MP3ID3Tag tag = new MP3ID3Tag(); // 클래스형인 tag 객체 초기화
-					fs.Seek(-128, SeekOrigin.End);
-					fs.Read(tag.TAGID, 0, tag.TAGID.Length);
-					fs.Read(tag.Title, 0, tag.Title.Length);
-					fs.Read(tag.Artist, 0, tag.Artist.Length);
-					fs.Read(tag.Album, 0, tag.Album.Length);
-					fs.Read(tag.Year, 0, tag.Year.Length);
-					fs.Read(tag.Comment, 0, tag.Comment.Length);
-					fs.Read(tag.Genre, 0, tag.Genre.Length);
-					string theTAGID = Encoding.Default.GetString(tag.TAGID);
-
-					if (theTAGID.Equals("TAG"))
+					if (fs.Length >= 128)
 					{
-						string Title = Encoding.Default.GetString(tag.Title).Trim('\0');
-						string Artist = Encoding.Default.GetString(tag.Artist).Trim('\0');
-						string Album = Encoding.Default.GetString(tag.Album).Trim('\0');
-						string Year = Encoding.Default.GetString(tag.Year).Trim('\0');
-						string Comment = Encoding.Default.GetString(tag.Comment).Trim('\0');
-						string Genre = Encoding.Default.GetString(tag.Genre).Trim('\0');
+						MP3ID3Tag tag = new MP3ID3Tag(); // 클래스형인 tag 객체 초기화
+						fs.Seek(-128, SeekOrigin.End);
+						fs.Read(tag.TAGID, 0, tag.TAGID.Length);
+						fs.Read(tag.Title, 0, tag.Title.Length);
+						fs.Read(tag.Artist, 0, tag.Artist.Length);
+						fs.Read(tag.Album, 0, tag.Album.Length);
+						fs.Read(tag.Year, 0, tag.Year.Length);
+						fs.Read(tag.Comment, 0, tag.Comment.Length);
+						fs.Read(tag.Genre, 0, tag.Genre.Length);
+						string theTAGID = Encoding.Default.GetString(tag.TAGID);
 
-						if (string.IsNullOrEmpty(Title))
-							Title = GetSongTitleFromFilePath(filePath);
-						Song song = new Song(Album, Artist, Title, filePath, null);
+						if (theTAGID.Equals("TAG"))
+						{
+							string Title = Encoding.Default.GetString(tag.Title).Trim('\0');
+							string Artist = Encoding.Default.GetString(tag.Artist).Trim('\0');
+							string Album = Encoding.Default.GetString(tag.Album).Trim('\0');
+							string Year = Encoding.Default.GetString(tag.Year).Trim('\0');
+							string Comment = Encoding.Default.GetString(tag.Comment).Trim('\0');
+							string Genre = Encoding.Default.GetString(tag.Genre).Trim('\0');
 
-						return song;
+							if (string.IsNullOrEmpty(Title))
+								Title = GetSongTitleFromFilePath(filePath);
+							Song song = new Song(Album, Artist, Title, filePath, null);
+
+							return song;
+						}
+						else
+							return null;
 					}
 					else
 						return null;
 				}
-				else
-					return null;
+			}
+			catch (FileNotFoundException e)
+			{
+				Debug.WriteLine(e.Message);
+				return null;
+			}
+			catch(System.Exception e)
+			{
+				Debug.WriteLine(e.Message);
+				return null;
 			}
 		}
 
